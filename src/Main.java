e3dcda6a282677c9eb9563952e1d377463a78d9f
Cyriac-Ernest <103205781+cyriac9966@users.noreply.github.com>
2023-01-24 20:07:33 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         ShoppingList shoppingList = new ShoppingList();
        shoppingList.shopApp();
-        //shoppingList.terminateProgram();
+        
 
     }
 }
